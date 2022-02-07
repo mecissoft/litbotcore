0c50c28830f5b436862b7654991fc35d06d86113
mecissoft <93204517+mecissoft@users.noreply.github.com>
2022-02-07 15:24:56 +0200

    Add files via upload

---
+++ core.py
@@ -0,0 +1,58 @@
+import discord 
+from discord.ext import commands
+import json
+import requests
+import asyncio
+client = commands.Bot(command_prefix = "!")
+
+@client.event
+
+async def on_ready():
+	print("Bot Is Started!")
+
+async def ver ( ctx ):
+    await ctx.send('Ubutal Bot Core 1.6. Based On Ubutal Bot 1.6')
+    
+@client.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandNotFound ):
+        await ctx.send('Command Not Found.')
+
+@client.command()
+async def rcat(ctx):
+    response = requests.get("https://some-random-api.ml/img/cat")
+    json_data = json.loads(response.text)
+
+    s = discord.Embed(color = 0xff9900, title = 'Random Cat')
+    s.set_image(url = json_data['link'])
+    await ctx.send(embed = s)
+
+@client.event
+async def on_ready():
+     while True:
+          await client.change_presence(status=discord.Status.online, activity=discord.Game("UBC Based"))
+
+@client.command(name='say')
+async def audit(ctx, *, msg=None):
+    if msg is not None:
+        await ctx.send(msg)
+
+@client.command()
+async def avatar(ctx, *,  avamember : discord.Member=None):
+    userAvatarUrl = avamember.avatar_url
+    await ctx.send(userAvatarUrl)
+
+@client.command(pass_context = True)
+async def ping( ctx ):
+    author = ctx.message.author 
+    await ctx.send(f'{author.mention}, Pong!')
+
+@client.command(pass_context = True)
+async def info( ctx ):
+ await ctx.send('This Bot Based On Ubutal Bot Core!')
+
+token = open("token.txt", "r").readline()
+
+client.run(token)
+
+print("OK")
